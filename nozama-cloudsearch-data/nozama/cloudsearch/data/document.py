796b9fa881ea859e168a11ad180ca384bdc963db
hokuma <okuma@hokuma.net>
2016-09-25 21:34:30 +0900

    return all fields
    for development, always return fields values

--- nozama-cloudsearch-data/nozama/cloudsearch/data/document.py
+++ nozama-cloudsearch-data/nozama/cloudsearch/data/document.py
@@ -149,13 +149,20 @@ def search(query={}):
             took=0,
         )
 
+    hit = []
+    conn = db().conn()
+    for i in results['hits']['hits']:
+        query = dict(_id=i['_id'])
+        fields = conn.documents.find_one(query)['fields']
+        hit.append({"id": i['_id'], "fields": fields})
+
     rc = {
         "rank": "-text_relevance",
         "match-expr": "(label '{0}')".format(qstring),
         "hits": {
             "found": results['hits']['total'],
             "start": 0,
-            "hit": [{"id": i['_id']} for i in results['hits']['hits']]
+            "hit": hit
         },
         "info": {
             "rid": os.urandom(40).encode('hex'),
